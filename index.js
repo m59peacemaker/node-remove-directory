22e4047169badbd8ad3af6bde712f137f74b57a0
Johnny Hauser <johnnyhauser@gmail.com>
2019-06-02 13:06:30 -0500

    support version of node before fs.promises was a thing

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-const fs = require('fs')
 const path = require('path')
+const { readdir, unlink, rmdir }  = require('fs')
+const { promisify } = require('util')
+const readdirAsync = promisify(readdir)
+const unlinkAsync = promisify(unlink)
+const rmdirAsync = promisify(rmdir)
 
 const removeDirectory = async directoryPath => {
-	const entries = await fs.promises.readdir(directoryPath, { withFileTypes: true })
+	const entries = await readdirAsync(directoryPath, { withFileTypes: true })
 	await Promise.all(entries.map(entry => {
 		const entryPath = path.join(directoryPath, entry.name)
-		return entry.isDirectory() ? removeDirectory(entryPath) : fs.promises.unlink(entryPath)
+		return entry.isDirectory() ? removeDirectory(entryPath) : unlinkAsync(entryPath)
 	}))
-	return fs.promises.rmdir(directoryPath)
+	return rmdirAsync(directoryPath)
 }
 
 module.exports = removeDirectory
