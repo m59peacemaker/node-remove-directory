88bbec96a505a43a243c5ccf1a91bae26bf66969
Johnny Hauser <johnnyhauser@gmail.com>
2019-06-02 15:45:33 -0500

    withFileTypes also not supported by old node, ofc

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const path = require('path')
-const { readdir, unlink, rmdir }  = require('fs')
+const { readdir, rmdir, stat, unlink }  = require('fs')
 const { promisify } = require('util')
 const readdirAsync = promisify(readdir)
 const unlinkAsync = promisify(unlink)
 const rmdirAsync = promisify(rmdir)
+const statAsync = promisify(stat)
 
 const removeDirectory = async directoryPath => {
-	const entries = await readdirAsync(directoryPath, { withFileTypes: true })
-	await Promise.all(entries.map(entry => {
-		const entryPath = path.join(directoryPath, entry.name)
+	const entries = await readdirAsync(directoryPath)
+	await Promise.all(entries.map(async name => {
+		const entryPath = path.join(directoryPath, name)
+		const entry = await statAsync(entryPath)
 		return entry.isDirectory() ? removeDirectory(entryPath) : unlinkAsync(entryPath)
 	}))
 	return rmdirAsync(directoryPath)
